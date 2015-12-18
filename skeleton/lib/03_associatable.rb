6129de98dc9b0b592ea4ebbfbe0e048e976d4d22
Michael Duke <michaelcduke@gmail.com>
2015-12-18 17:04:42 -0500

    Complete 04_associatable2.rb. All specs passing.

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -20,47 +21,52 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    if options == {}
-      @foreign_key = "#{name}_id".to_sym
-      @class_name = "#{name.capitalize}"
-      @primary_key = :id
-    else
-      @foreign_key = options[:foreign_key]
-      @class_name = options[:class_name]
-      @primary_key = options[:primary_key]
-    end
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+    @foreign_key ||= "#{name}_id".to_sym
+    @class_name ||= "#{name.capitalize}"
+    @primary_key ||= :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    if options == {}
-      @foreign_key = "#{self_class_name.downcase}_id".to_sym
-      @class_name = "#{name.capitalize.singularize}"
-      @primary_key = :id
-    else
-      @foreign_key = options[:foreign_key]
-      @class_name = options[:class_name]
-      @primary_key = options[:primary_key]
-    end
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+    @foreign_key ||= "#{self_class_name.downcase}_id".to_sym
+    @class_name ||= "#{name.to_s.capitalize.singularize}"
+    @primary_key ||= :id
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    assoc_options[name] = options
+
+    define_method(name) do
+      foreign_key = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key.to_sym => foreign_key).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+
+    define_method(name) do
+      fkey = options.foreign_key
+      options.model_class.where(fkey => self.id)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
--- skeleton/lib/04_associatable2.rb
+++ skeleton/lib/04_associatable2.rb
@@ -5,6 +5,36 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+      source_table_name = source_options.model_class.table_name #ok
+      thru_table_name = through_options.model_class.table_name #ok
+      source_fgn_key = source_options.foreign_key
+      join_line = "#{source_table_name} ON #{thru_table_name}.#{source_fgn_key} = #{source_table_name}.id"
+      searchid = self.send(through_options.foreign_key)
+      result = DBConnection.execute(<<-SQL, searchid)
+      SELECT
+        #{source_table_name}.*
+      FROM
+        #{thru_table_name}
+      JOIN
+        #{join_line}
+      WHERE
+        #{thru_table_name}.id = ?
+      SQL
+      source_options.model_class.parse_all(result).first
+    end
+
   end
 end
+
+# SELECT
+#   houses.*
+# FROM
+#   humans
+# JOIN
+#   houses ON humans.house_id = houses.id
+# WHERE
+#   humans.id = ?
