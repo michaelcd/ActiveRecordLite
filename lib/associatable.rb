388ae372569cfa48507876046b353ad400ee29f3
Michael Duke <michaelcduke@gmail.com>
2016-02-16 17:51:06 -0500

    Create active_record_lite.rb. Consolidate associatable into one file.

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -54,6 +54,31 @@ module Associatable
       options.model_class.where(foreign_key => self.id)
     end
   end
+  
+  def has_one_through(name, through_name, source_name)
+    define_method(name) do
+
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+      src_table = source_options.model_class.table_name
+      thru_table = through_options.model_class.table_name
+      src_fgn_key = source_options.foreign_key
+      join_line = "#{src_table} ON #{thru_table}.#{src_fgn_key} = #{src_table}.id"
+      searchid = self.send(through_options.foreign_key)
+
+      result = DBConnection.execute(<<-SQL, searchid)
+      SELECT
+        #{src_table}.*
+      FROM
+        #{thru_table}
+      JOIN
+        #{join_line}
+      WHERE
+        #{thru_table}.id = ?
+      SQL
+      source_options.model_class.parse_all(result).first
+    end
+  end
 
   def assoc_options
     @assoc_options ||= {}
